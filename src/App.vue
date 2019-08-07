1df27a4047fe059241b553f2bec8325acf5a920d
bchutchison <benhutchison85@gmail.com>
2019-08-07 16:43:36 +0100

    country click button addded

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div>
+    <h1>My Country List</h1>
+    <div class="main-container">
+      <countries-list :countries='countries'></countries-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import CountriesList from './components/CountriesList.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      countries: []
+    };
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+  },
+  components: {
+    "countries-list": CountriesList
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
